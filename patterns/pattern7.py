84821f221eb484fc2bd6c8aa334670fe9b147c28
Mayank Parikh <mayank.p@pickmywork.com>
2024-08-25 09:50:33 +0530

    Seventh Pattern

---
+++ patterns/pattern7.py
@@ -0,0 +1,12 @@
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+noOfRows = int(input("Enter number of rows: "))
+
+for row in range(1, noOfRows + 1):
+  count = 2 * row - 1
+  spaceCount = (noOfRows - row) * 2
+  print(" " * spaceCount, "* " * count)
