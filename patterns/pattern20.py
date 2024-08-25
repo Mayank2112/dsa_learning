9ac0a7c969acee8e3884c199b6f5a77f0ded4cd4
Mayank Parikh <mayankparikh21dec@gmail.com>
2024-08-25 17:21:47 +0530

    Twentyth Pattern

---
+++ patterns/pattern20.py
@@ -0,0 +1,21 @@
+# *        *
+# **      **
+# ***    ***
+# ****  ****
+# **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
+
+num = int(input("Enter a number: "))
+
+for i in range(1, num):
+  stars = "*" * i
+  spaces = "  " * (num - i)
+  print(stars + spaces + stars)
+
+for i in range(num, 0, -1):
+  stars = "*" * i
+  spaces = "  " * (num - i)
+  print(stars + spaces + stars)
