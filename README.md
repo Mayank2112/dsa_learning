fe41f79cac11b07319f1291046599a2c855e5c79
Mayank Parikh <mayankparikh21dec@gmail.com>
2024-08-28 20:26:19 +0530

    Greatest Common Divisor

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <h1>DSA Learning Journey</h1>
 1. Patterns
+2. Basic Maths
---
+++ basic_maths/GCD.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GCD {
+  static long findGCD(long num1, long num2) {
+    if (num2 < num1) {
+      long t = num1;
+      num1 = num2;
+      num2 = t;
+    }
+    long t = num1;
+    int count = 1;
+    while (t > 0) {
+      if (num1 % t == 0 && num2 % t == 0) return t;
+      count += 1;
+      t = (num1 / count) >= t ? t - 1 : (num1 / count);
+    }
+    return 1;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    long num1 = sc.nextLong();
+    System.out.print("Enter second number: ");
+    long num2 = sc.nextLong();
+    System.out.println(findGCD(num1, num2));
+  }
+}
